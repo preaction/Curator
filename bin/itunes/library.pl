d17f46499cf7384ee8805ef894746f0272dbea4e
Doug Bell <madcityzen@gmail.com>
2014-09-13 23:58:40 -0500

    add script to get tracks out of itunes library

---
+++ bin/itunes/library.pl
@@ -0,0 +1,15 @@
+
+use strict;
+use warnings;
+use feature qw( :5.10 );
+use Mac::PropertyList qw( parse_plist_file );
+use YAML ();
+
+# The XML iTunes Library has its information in a dictionary key called
+# "Tracks".
+#
+# It's also huge. Might need to do something a bit more custom here...
+
+my $library = parse_plist_file( shift @ARGV );
+say YAML::Dump( $_ ) for values %{ $library->as_perl->{Tracks} };
+
