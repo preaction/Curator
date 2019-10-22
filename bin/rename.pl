8ae30e9cd85769e72c28d0b2dfe165d6393b3787
Doug Bell <doug@preaction.me>
2019-10-22 08:51:53 -0500

    automatically add to TV app and organize better

--- bin/rename.pl
+++ bin/rename.pl
@@ -2,10 +2,17 @@
 use v5.24;
 use MP4::Info ();
 use Path::Tiny qw( path );
+use FindBin qw( $Bin );
+use autodie qw( :all );
 
 my $FILE_EXT = qr{mp4|m4v}i;
 my $DEST_ROOT = path( shift @ARGV );
+my $TV_ROOT = $DEST_ROOT->child( 'TV' );
+my $MOVIE_ROOT = $DEST_ROOT->child( 'Movies' );
+
 die "$DEST_ROOT does not exist" unless $DEST_ROOT->exists;
+die "$TV_ROOT does not exist" unless $TV_ROOT->exists;
+die "$MOVIE_ROOT does not exist" unless $MOVIE_ROOT->exists;
 if ( !@ARGV ) {
     unshift @ARGV, $DEST_ROOT;
 }
@@ -30,10 +37,11 @@ sub rename_file {
     my $ext = $1;
 
     my $info = MP4::Info->new( "$path" );
-    # ; say $path;
-    # ; use Data::Dumper;
-    # ; delete $info->{COVR};
-    # ; say Dumper $info;
+
+    # Sanity checks to make sure this is actually complete metadata
+    if ( grep !defined, $info->@{qw( TITLE ARTIST YEAR GENRE META )} ) {
+        next;
+    }
 
     my $title = $info->{TITLE} || $info->{NAM};
     $title =~ s/:/ -/g;
@@ -42,24 +50,44 @@ sub rename_file {
     my ( $season ) = $info->{ALB} =~ /Season (\d+)/;
     my $show = ucfirst $info->{ART};
 
-    next unless $title;
+    # ; say $path;
+    # ; use Data::Dumper;
+    # ; delete $info->{COVR};
+    # ; say Dumper $info;
+    # ; next;
 
     my $move_to;
     if ( $season ) {
         $show =~ s/\s+$//;
         $show =~ s/:/ -/g;
         my $name = sprintf '%s/%dx%02d %s.%s', $show, $season, $epnum, $title, $ext;
-        $move_to = $DEST_ROOT->child( $name );
+        $move_to = $TV_ROOT->child( $name );
         my $dir = $move_to->parent;
         $dir->mkpath unless $dir->is_dir;
     }
     else {
         my $name = sprintf '%s.%s', $title, $ext;
         $name =~ s/:/ -/g;
-        $move_to = $DEST_ROOT->child( $name );
+        $move_to = $MOVIE_ROOT->child( $name );
+    }
+
+    next if $path eq $move_to;
+    if ( -e $move_to ) {
+        my $dup_info = MP4::Info->new( "$move_to" );
+        # Check these fields for equality
+        my @fields = qw( TITLE NAM TRACKNUM ALB ART YEAR );
+        if ( !grep { $dup_info->{$_} ne $info->{$_} } @fields ) {
+            say "\t$move_to already exists and has the same metadata... Skipping.";
+            return;
+        }
+        $move_to = path( $move_to =~ s/([.][^.]+)$/ ($info->{YEAR})$1/r );
     }
 
     say "$path -> $move_to";
-    $path->move( $move_to );
+    my $copy = $path->copy( $move_to );
+    if ( $copy && $path ne $move_to ) {
+        $path->remove;
+        system path( $Bin, 'itunes', 'add-to-library.applescript' ), "$copy";
+    }
 }
 
